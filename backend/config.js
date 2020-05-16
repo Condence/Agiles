a0ae17bd6519dd24e7af8dd8a3fabb8bb4b7d591
AlphaZegnio <49506103+AlphaZegnio@users.noreply.github.com>
2020-05-16 12:56:53 -0700

    commit

--- backend/config.js
+++ backend/config.js
@@ -1,13 +1,7 @@
 module.exports = {
     mongodb: {
         defaultconnection: "mongodb://127.0.0.1:27017/SPA"
-    },
-    mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'Faceboot'
-    },
+    }, 
     auth: {
         secret: 'p1nch1p4l4br4s3cr3t4alv',
         rounds: 10
--- backend/core/persistence/connection/connection.js
+++ backend/core/persistence/connection/connection.js
@@ -22,17 +22,4 @@ mongoose.connection.on("disconnected", ()=>{
     logger.info(`Mongoose disconnected`);
 });
 // MYSQL 
-
-const connection_mysql = mysql.createConnection({
-    host: config.mysql.host,
-    user: config.mysql.user,
-    password: config.mysql.password,
-    database: config.mysql.database
-});
-connection_mysql.connect((err) => {
-    if (err) {
-        throw err;
-    } 
-    logger.info('Connected to database mysql');
-});
-global.connection_mysql = connection_mysql;
+ 
